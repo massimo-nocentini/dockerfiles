b65dcf926a09eb7b3406393089385515d672b37c
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-03-08 10:29:47 +0100

    Some updates to maintain working container

--- hyperledger-firefly/script.sh
+++ hyperledger-firefly/script.sh
@@ -1,4 +1,13 @@
 
-dockerd &
+#trap "echo 'Trapping SIGINT'; ff stop smartrepair" SIGINT
+#trap "echo 'Trapping SIGTERM'; ff stop smartrepair" SIGTERM
+
+dockerd & # start the Docker daemon
+dockerd_pid=$!
+sleep 5 # to let dockerd start
+ff stop smartrepair
 ff start smartrepair
+
 /bin/sh
+
+ff stop smartrepair
